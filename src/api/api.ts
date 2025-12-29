b548eab84c662dfc1b54eeced90e77091031fe65
Galaxy-peng <1094425279@qq.com>
2025-12-29 22:29:25 +0800

    commit

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://192.168.3.132:8080/api";
 
 export const apiFetch = async (
   url: string,
